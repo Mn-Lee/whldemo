3e0e028c9922c458c49d490c075ad7f8b413d284
Mn <3404906301@qq.com>
2019-05-24 21:02:34 +0800

    init repo

---
+++ src/main/java/com/whl/demo/WhldemoApplication.java
@@ -0,0 +1,13 @@
+package com.whl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhldemoApplication.class, args);
+    }
+
+}
