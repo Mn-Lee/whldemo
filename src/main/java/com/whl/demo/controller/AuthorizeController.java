a4c098ab72087ce7975fc2632f113238c8c8101a
Mn-Lee <3404906301@qq.com>
2019-06-04 22:31:02 +0800

    用session完成登陆状态暂存；配置mysql；注入spring-mybatis依赖

--- src/main/java/com/whl/demo/controller/AuthorizeController.java
+++ src/main/java/com/whl/demo/controller/AuthorizeController.java
@@ -4,9 +4,12 @@ import com.whl.demo.dto.AccessTokenDTO;
 import com.whl.demo.dto.GithubUserDTO;
 import com.whl.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -14,15 +17,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
+    //如果有request参数,spring会自动放入
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("491096625fc95d5bb1bb");
-        accessTokenDTO.setClient_secret("f4e9cf03768d077a43e3447173c2e932f41c19c8");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
@@ -30,6 +42,14 @@ public class AuthorizeController {
 
         System.out.println(user.getId() + " --- " + user.getLogin() + " --- " + user.getName());
 
-        return "index";
+        if(user!=null){
+            //登陆成功，写cookie, session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登陆失败
+            return "redirect:/";
+        }
+
     }
 }
